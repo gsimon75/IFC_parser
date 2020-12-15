91b92632e651f57b2aad2146dbe0b7d5f72f03f5
garbaczp <garbaczp@borogroup.co.uk>
2020-12-15 15:06:13 +0000

    setup and requirement files added
    Signed-off-by: garbaczp <garbaczp@borogroup.co.uk>

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='ifc_parser',
+    version='1.0',
+    description='IFC Parser',
+    author='Pawel Garbacz',
+    author_email='garbaczp@borogroup.co.uk',
+    url='https://github.com/boro-alpha/IFC_parser.git',
+    packages=setuptools.find_packages(),
+    include_package_data=True)
